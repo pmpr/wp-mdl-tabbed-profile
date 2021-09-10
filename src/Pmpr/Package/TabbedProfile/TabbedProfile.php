33639e160fd2ac4bc3dfd14c0a86898baa99d224
Pmnmpr <pmnmpr@gmail.com>
2021-09-10 20:02:08 +0000

    Obfuscated

--- src/Pmpr/Package/TabbedProfile/TabbedProfile.php
+++ src/Pmpr/Package/TabbedProfile/TabbedProfile.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             613a0148de3f5             |
+    |             613bb99ac92d2             |
     |_______________________________________|
 */
 
@@ -14,48 +14,22 @@ class TabbedProfile extends Container
     
     public function __construct()
     {
-        goto qiaqsassksqiuyae;
-        qiaqsassksqiuyae:
         $this->gkieogwukagigisy(__DIR__);
-        goto qogqewiwmwiwskgm;
-        qogqewiwmwiwskgm:
-        if (!$this->gmiyqqaekqcsoime()) {
-            goto cecuyayqoioasumi;
+        if ($this->gmiyqqaekqcsoime()) {
+            parent::__construct();
         }
-        goto qgoiooayqmqqsiok;
-        qgoiooayqmqqsiok:
-        parent::__construct();
-        goto qwigomakwmyiwkgo;
-        qwigomakwmyiwkgo:
-        cecuyayqoioasumi:
-        goto myoicgcuugciueis;
-        myoicgcuugciueis:
     }
     public function wigskegsqequoeks()
     {
-        $this->qcsmikeggeemccuu("\x61\x64\x6d\151\156\x5f\151\x6e\151\x74", [$this, "\153\x6b\165\x63\x6d\x63\x61\x61\x79\141\151\x65\x61\163\171\151"]);
+        $this->qcsmikeggeemccuu('admin_init', [$this, 'kkucmcaayaieasyi']);
         parent::wigskegsqequoeks();
     }
     public function kkucmcaayaieasyi()
     {
-        goto csscmcacoikwsecs;
-        mkwskuycuyguqqok:
-        $eygsasmqycagyayw->ikqyiskqaaymscgw("\x65\x78\164\x72\x61", ["\151\x64" => "\x6d\x69\163\x63\x65\154\154\x61\156\145\x6f\165\163", "\x74\x69\164\154\145" => __("\115\151\163\143\x65\154\154\x61\156\x65\x6f\165\163", PR__PKG__TABBED_PROFILE)]);
-        goto sciwggaeogcoesiu;
-        csscmcacoikwsecs:
-        if (!ManipulateUser::eoyueosccuoeqkee()) {
-            goto asmecuqiyyswueqe;
+        if (ManipulateUser::eoyueosccuoeqkee()) {
+            $eygsasmqycagyayw = $this->miocmcoykayoyyau();
+            $eygsasmqycagyayw->ayeieigcckcmsikq($eygsasmqycagyayw->owygwqwawqoiusis('admin', $eygsasmqycagyayw->get('admin.js')));
+            $eygsasmqycagyayw->ikqyiskqaaymscgw('extra', ['id' => 'miscellaneous', 'title' => __('Miscellaneous', PR__PKG__TABBED_PROFILE)]);
         }
-        goto cuykwgmswkskqkyi;
-        kuicqywysciceggs:
-        $eygsasmqycagyayw->ayeieigcckcmsikq($eygsasmqycagyayw->owygwqwawqoiusis("\141\144\155\x69\x6e", $eygsasmqycagyayw->get("\x61\144\155\151\156\56\152\x73")));
-        goto mkwskuycuyguqqok;
-        sciwggaeogcoesiu:
-        asmecuqiyyswueqe:
-        goto eqkauqciwewmgeoi;
-        cuykwgmswkskqkyi:
-        $eygsasmqycagyayw = $this->miocmcoykayoyyau();
-        goto kuicqywysciceggs;
-        eqkauqciwewmgeoi:
     }
 }
